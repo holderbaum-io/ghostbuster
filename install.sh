f000354df6970ac74860e25b3fe3f9e6c17e6e6f
Jakob Holderbaum <jakob@holderbaum.io>
2019-01-09 10:46:37 +0100

    Implement simple install script

--- install.sh
+++ install.sh
@@ -6,4 +6,28 @@ info() {
   printf "\033[0;32m%s\033[0m\n" "$1"
 }
 
-info 'Installing Ghostbuster'
+error() {
+  printf "\033[0;31m%s\033[0m\n" "$1"
+}
+
+target=/usr/local/bin/ghostbuster
+
+if [ -e "$target" ];
+then
+  if [ "$(tail -n1 "$target")" = '# LAST LINE TO DETECT PREVIOUS GHOSTBUSTER' ];
+  then
+    info 'Updating Ghostbuster'
+  else
+    error "Can't install Ghostbuster, somthing with this is name is already installed"
+    exit 1
+  fi
+else
+  info 'Installing Ghostbuster'
+fi
+
+
+ghostbuster="$(mktemp)"
+url='https://raw.githubusercontent.com/hrx-events/ghostbuster/master/ghostbuster'
+curl -sSl "$url" >"$ghostbuster"
+sudo mv "$ghostbuster" "$target"
+sudo chmod +x "$target"
